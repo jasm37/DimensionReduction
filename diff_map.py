e3af586932c24b90d5ec601d4a365499abb2f5aa
Jorge <jasm915@gmail.com>
2017-07-07 16:24:39 +0200

    Added Laplace Beltrami approximation and fixed dim_reduction

--- diff_map.py
+++ diff_map.py
@@ -1,8 +1,8 @@
 import numpy as np
 import numpy.linalg as LA
 from scipy.spatial import distance
-from scipy.sparse.linalg import eigs
-
+from scipy.sparse.linalg import eigsh
+from sklearn.neighbors import NearestNeighbors
 
 class DiffusionMap:
     def __init__(self, eps, step):
@@ -14,11 +14,13 @@ class DiffusionMap:
         return distance.cdist(data, data, 'euclidean')
 
     def get_kernel(self, data, eps):
-        # TODO: use scikit to complete the distances(this may be faster)
+        # TODO: use scikit to complete the distances(may be faster)
         D = distance.cdist(data,data,'euclidean')
         return np.exp(-D ** 2 / eps)
 
     def get_partial_kernel(self, data, delta, param):
+        # TODO implement separate knn and epsilon-nbhd function to create the adjacency matrix
+        # TODO compare current computation of knn with NearestNeighbourhood from scikitlearn
         # Computes kernel matrix based on eps or k-nbhd
         # delta is the epsilon of diffusion maps
         num_samples = data.shape[0]
@@ -45,28 +47,20 @@ class DiffusionMap:
     def get_diffusion_map(self, data, eps, param = None):
         # Method could also be named "get_diffusion_map"
         if param:
-            H = self.get_partial_kernel(data, eps, param)
+            W = self.get_partial_kernel(data, eps, param)
         else:
-            H = self.get_kernel(data, eps)
-        #H is the kernel matrix
-        # TODO : normalization is missing(Laplace Beltrami)
-        # Normalization:
+            W = self.get_kernel(data, eps)
+        # W is the kernel/weight matrix
+        # Approximates Laplace Beltrami operator: alpha = 1 in Lafon's paper
+        vd = np.sum(W, axis=1)
+        mult = np.outer(vd,vd)
+        W = W/mult
+        vd = np.sum(W, axis=1)
+        mult = np.sqrt(np.outer(vd,vd))
+        M = W/mult
 
-        #v = np.sum(H, axis=0)
-        #w = np.sum(H, axis=1)
-        #mult = 1./np.outer(w, v.T)
-        #M = H/v[:,np.newaxis]
-        #sqrt_v = np.sqrt(v)
-        #den = np.outer(sqrt_v,sqrt_v)
-        #M = G/den
-        '''
-        v = 1./np.sqrt(np.sum(M, axis=0))
-        w = 1./np.sqrt(np.sum(M, axis=1))
-        mult = np.outer(w, v.T)
-        M = np.dot(M, mult)
-        '''
-        #M is a markov chain
-        M = (H.T / H.sum(axis=1)).T
+        #This M is a markov chain
+        #M = (W.T / W.sum(axis=1)).T
         return M
 
     def dim_reduction(self, data, ndim, param = None):
@@ -74,17 +68,28 @@ class DiffusionMap:
         #           ndim:   desired dimension (ndim<d)
         #           param: contains info about nbhd type for adjancency matrix
         #               knn or eps-nbhd
-        # OUTPUT:   (N,ndim) array with reduced dimension
+        # OUTPUT:   first 'ndim' nontrivial eigenvalues and (N,ndim) array
+        #           with reduced dimension
 
         H = self.get_diffusion_map(data, self.eps, param)
         # We get the first ndim+1 eigenvalues and discard the first one since it is 1
-        w, x = eigs(H, k=ndim+1 , which='LM')
-        w = w[1:ndim+1]
-        print x[:, 0] / x[:,2]
+        w, x = eigsh(H, k=ndim+1 , which='LM', ncv=None)
+        # Get decreasing order of evectors and evalues
+        w = w[::-1]
+        x = x[:,::-1]
         # Compute data reduction according to Coifman and Lafon :
         # http://www.sciencedirect.com/science/article/pii/S1063520306000546
         # Note: take real part in case of any complex eigenvector/value
-        x = (w.real ** self.step) * x[:, 1:ndim+1].real
-        #print x[:,0]
-        #x = (x.T/x[:,0]).T
+        x = (w[1:ndim+1]**self.step) * x[:, 1:ndim+1]
         return w, x
+
+
+    def get_distance_matrix_and_k(self, data, k):
+        print 'Using get_distance_matrix_and_k for testing only'
+        # instead of using euclidean, sqeuclidean may be slightly faster
+        D = distance.cdist(data,data, 'sqeuclidean')
+        k_nbhd = NearestNeighbors(n_neighbors=k )
+        k_nbhd.fit(data)
+        distances, indices = k_nbhd.kneighbors()
+        #change distance to double/float
+        return D, indices, distances
