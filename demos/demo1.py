8d6c2f67a958e4a9df29c54d1fb2b3358fdbcbed
Jorge <jasm915@gmail.com>
2017-07-10 15:33:54 +0200

    Added comments and fixed code

--- demos/demo1.py
+++ demos/demo1.py
@@ -6,31 +6,35 @@ from gen_data import get_data
 
 if __name__ == '__main__':
 
-    A, color = get_data('gaussian', 1500)
-    print A.shape
-    print color.shape
+    # Simple script to test diff. maps dimension reduction
+    samples = 'gaussian'
+    num_samples = 1500
+    A, color = get_data(samples, num_samples)
 
     fig = plt.figure(figsize=(10, 5))
     ax = fig.add_subplot(121, projection='3d')
     ax.scatter(A[:,0], A[:,1], A[:,2], c = color, cmap = plt.cm.Spectral)
-
+    plt.title('Original data')
     # target dimension
     ndim = 2
     # parameters for diff. maps
     step = 2
-    eps = 10
+    eps = 0
     # params for k nearest neighbourhood
     # params for eps nearest neighbourhood(epsilon param depends highly on the dataset)
-    param = {'k': 50}
-    # param = {'eps': 1.75}
+    nbhd_param = {'k': 50}
+    # nbhd_param = {'eps': 1.75}
 
-    diff_map = DiffusionMap(eps, step)
-    w, x = diff_map.dim_reduction(A, ndim, param=param)
+    diff_map = DiffusionMap(step, eps)
+    diff_map.set_params(A, nbhd_param)
+    w, x = diff_map.dim_reduction(ndim)
     #w, x = diff_map.dim_reduction(A, ndim)
 
     ax = fig.add_subplot(122)
     plt.scatter(x[:, 0], x[:, 1], c=color, cmap=plt.cm.Spectral)
-
+    plt.xlabel('phi_1')
+    plt.ylabel('phi_2')
+    plt.title('2D representation with Diff. Maps')
     plt.show()
 
     '''
--- demos/demo2.py
+++ demos/demo2.py
@@ -6,8 +6,13 @@ from diff_map import DiffusionMap
 from gen_data import get_data
 from sklearn.decomposition import PCA
 
-A, color = get_data('two_planes', 1000)
+# Compares Diff. Map dim. reduction and PCA
 
+# Pick here which data to generate for the coomparison
+sample = 'gaussian'
+A, color = get_data(sample, 1000)
+
+# Plot original 3D data
 fig = plt.figure(figsize=(13, 5))
 ax = fig.add_subplot(131, projection='3d')
 ax.set_title("Original 3D data")
@@ -16,25 +21,26 @@ ax.scatter(A[:,0], A[:,1], A[:,2], c = color, cmap = plt.cm.Spectral)
 # target dimension
 ndim = 2
 # parameters for diff. maps
-step = 2
-eps = 10
+step = 1
+eps = 0
 # params for k nearest neighbourhood
 # params for eps nearest neighbourhood(epsilon param depends highly on the dataset)
-param = {'k': 50}
+#nbhd_param = {'k': 50}
 # param = {'eps': 1.75}
 
-diff_map = DiffusionMap(eps, step)
-#w, x = diff_map.dim_reduction(A, ndim)
-w, x = diff_map.dim_reduction(A, ndim, param)
+# Diff. Map computation and plot
+diff_map = DiffusionMap(step, eps)
+#diff_map.set_params(A, nbhd_param)
+diff_map.set_params(A)
+w, x = diff_map.dim_reduction(ndim)
 
 ax = fig.add_subplot(132)
 ax.set_title("Diffusion Maps")
 plt.scatter(x[:, 0], x[:, 1], c=color, cmap=plt.cm.Spectral)
 
+# PCA computation and plot
 pca = PCA(n_components=2)
 U =pca.fit_transform(A)
-
-print U.shape
 
 ax = fig.add_subplot(133)
 ax.set_title("PCA")
