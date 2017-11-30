de490ab7b12f5420c7f863ed776b623d5e8d1c92
Jorge <jasm915@gmail.com>
2017-11-30 17:32:39 +0100

    demo6.py now works properly with parsimonious representation

--- demos/demo6.py
+++ demos/demo6.py
@@ -3,47 +3,45 @@ from mpl_toolkits.mplot3d import Axes3D
 from diff_map import DiffusionMap
 from gen_data import get_data
 from pars_rep_dm import compute_res
+
 import numpy as np
 # Demo to check parsimonious representation of eigenvectors/values
 
-sample = 'swiss'
-A, color = get_data(sample, 5000)
+sample = 'punc_sphere'
+A, color = get_data(sample, 1000)
+
+# target dimension
+# For plotting reasons, ndim should be bigger than 5
+ndim = 6
+
+# Diff. Map computation and plot
+diff_map = DiffusionMap()
+diff_map.set_params(A)
+w, x = diff_map.dim_reduction(ndim=ndim)
 
 # Plot original 3D data
 fig = plt.figure(figsize=(12, 8))
 ax = fig.add_subplot(231, projection='3d')
 ax.set_title("Original 3D data")
-ax.scatter(A[:,0], A[:,1], A[:,2], c = color, cmap = plt.cm.Spectral)
-
-# target dimension
-ndim = 2
-# parameters for diff. maps
-step = 0.005
-# when eps = 0 then the class computes an appropriate eps according to the dataset
-eps = 0.1
-
-# params for k nearest neighbourhood
-# params for eps nearest neighbourhood(epsilon param depends highly on the dataset)
-#nbhd_param = {'k': 10}
-#nbhd_param = {'eps': 0.1}
-
-# Diff. Map computation and plot
-diff_map = DiffusionMap()
-diff_map.set_params(A)
-w, x = diff_map.dim_reduction(ndim)
+ax.scatter(A[:,0], A[:,1], A[:,2], c=x[:,0], cmap = plt.cm.Spectral)
 
 ax = fig.add_subplot(232)
 ax.set_title("Diffusion Maps")
 plt.scatter(x[:, 0], x[:, 1], c=color, cmap=plt.cm.Spectral)
-w, x = diff_map.dm_basis(n_components=4)
+min_x = min(x[:,0]) - 0.2*np.abs(min(x[:,0]))
+min_y = min(x[:,1]) - 0.2*np.abs(min(x[:,1]))
+max_x = max(x[:,0]) + 0.2*np.abs(max(x[:,0]))
+max_y = max(x[:,1]) + 0.2*np.abs(max(x[:,1]))
+plt.axis([min_x, max_x, min_y, max_y])
 
-eps_scale = 0.1
-res = np.squeeze(compute_res(x[:10].T, eps_scale))
-print("res computed,",res)
+
+eps_scale = 5
+res,_ = compute_res(x[:,:ndim], eps_scale)
+print("Computed residuals are :",res)
 indices = np.argsort(res)
 indices = indices[::-1]
-print("res sorted,",res[indices])
-y = diff_map.param_from_indices(indices, ndim=2)
+print("Sorted residuals are :",res[indices])
+y = diff_map.param_from_indices(indices, ndim=ndim)
 
 ax = fig.add_subplot(233)
 ax.set_title("Order of eigvec")
@@ -52,19 +50,19 @@ plt.scatter(y[:, 0], y[:, 1], c=color, cmap=plt.cm.Spectral)
 ax = fig.add_subplot(234)
 ax.set_title("C1 vs C2")
 indices = [0,1]
-x = diff_map.param_from_indices(indices, ndim=2)
+x = diff_map.param_from_indices(indices, ndim=ndim)
 plt.scatter(x[:, 0], x[:, 1], c=color, cmap=plt.cm.Spectral)
 
 ax = fig.add_subplot(235)
 ax.set_title("C1 vs C3")
 indices = [0,2]
-y = diff_map.param_from_indices(indices, ndim=2)
+y = diff_map.param_from_indices(indices, ndim=ndim)
 plt.scatter(y[:, 0], y[:, 1], c=color, cmap=plt.cm.Spectral)
 
 ax = fig.add_subplot(236)
 ax.set_title("C2 vs C5")
 indices = [1,4]
-z = diff_map.param_from_indices(indices, ndim=2)
+z = diff_map.param_from_indices(indices, ndim=ndim)
 plt.scatter(z[:, 0], z[:, 1], c=color, cmap=plt.cm.Spectral)
 
 plt.show()
