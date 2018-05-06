26995e6f659a2ca10d00c2a672d0f4a37d6b3cb3
Jorge <jasm915@gmail.com>
2018-05-06 03:29:10 +0200

    Fixed swiss example

--- demos/demo8.py
+++ demos/demo8.py
@@ -11,22 +11,13 @@ from pars_rep_dm import compute_res
 ## and main matlab code :
 ##  http://ronen.net.technion.ac.il/files/2016/07/DsilvaACHA.zip
 
-sample = 'gaussian'
-data, color = get_data(sample, 2000)
-'''
-A = np.random.rand(500,3)
-sc_x = 4
-sc_y = 1
-A[:,0] = sc_x * A[:,0]
-A[:,1] = sc_y * A[:,1]
-A[:,2] = 3
-data = A
-color = data[:,0]
-'''
+sample = 'two_planes'
+data, color = get_data(sample, 1000)
+color = np.squeeze(color)
 
 ndim = 10
 step = 1
-eps=0
+eps = 0
 
 # DM constructor and main parameters
 diff_map = DiffusionMap(step, eps)
@@ -40,10 +31,10 @@ eps_med_scale = 5
 # Compute residuals given the eigvectors
 RES, _ = compute_res(V, eps_med_scale)
 
-print("Unsorted Residuals: ",RES)
+#print("Unsorted Residuals: ",RES)
 indices = np.argsort(RES)
 indices = indices[::-1]
-print("Sorted Residuals: ",RES[indices])
+#print("Sorted Residuals: ",RES[indices])
 
 # Reorder eigvalues and eigvectors according to residuals
 DD = D[indices]
